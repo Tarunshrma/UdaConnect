def2fe38a5c300041b8e17056377515bc37e351c
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-01-29 13:20:57 +0400

    Added location resource to opeapi namespace

--- modules/location-api/producer/app.py
+++ modules/location-api/producer/app.py
@@ -5,15 +5,15 @@ import os
 
 from kafka import KafkaProducer
 from flask import Flask, jsonify, request, Response
-from flask_restx import Resource, Api
+from flask_restx import Resource, Api, Namespace
 from flask_accepts import accepts, responds
 from schema import LocationSchema
 
 app = Flask(__name__)
 api = Api(app, title="UdaConnect Location API")
 
-# namespace = Namespace("udaconnect.location", description="udaconnect location api")  # noqa
-# api.add_namespace(namespace, path=f"/")
+namespace = Namespace("udaconnect.location", description="udaconnect location api")  # noqa
+api.add_namespace(namespace, path=f"/api/v1/")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("udaconnect-location-api")
@@ -24,10 +24,6 @@ KAFKA_SERVER = os.environ["KAFKA_SERVER_URL"]
 
 kafka_producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
 
-# @app.before_request
-# def before_request():
-#     # Set up a Kafka producer
-
 
 @api.route('/health')
 class Health(Resource):
@@ -35,20 +31,21 @@ class Health(Resource):
         return jsonify({'response': 'Healthy'})
 
 
-@app.route('/api/v1/locations', methods=['POST'])
-@accepts(schema=LocationSchema)
-@responds(201, 'Location Created')
-@responds(400, 'Invalid Location Data')
-def locations():
-    request_body = request.json
-    kafka_data = json.dumps(request_body).encode()
-    kafka_producer.send(TOPIC_NAME, kafka_data)
-    kafka_producer.flush()
+@api.route('/locations', methods=['POST'])
+class Locatations(Resource):
+    @accepts(schema=LocationSchema)
+    @responds(201, 'Location Created')
+    @responds(400, 'Invalid Location Data')
+    def locations():
+        request_body = request.json
+        kafka_data = json.dumps(request_body).encode()
+        kafka_producer.send(TOPIC_NAME, kafka_data)
+        kafka_producer.flush()
 
-    logger.info(
-        f"location data to be submitted: {request_body}")
+        logger.info(
+            f"location data to be submitted: {request_body}")
 
-    return Response(status=201)
+        return Response(status=201)
 
 
 # if __name__ == '__main__':
